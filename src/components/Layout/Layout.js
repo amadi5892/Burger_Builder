2762a86334524064c4eb343d9c8eafc541274da6
Parrish Williamson <williamsonparrish@gmail.com>
2021-01-27 17:25:11 -0500

    layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+
+const layout = (props) => (
+    <Auxillary>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>
+            {props.children}
+        </main>
+    </Auxillary>
+);
+
+export default layout;
